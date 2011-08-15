994be289686b6daed5ce914b46094682ac602476
Chris Morgan <thecapn.morgan@gmail.com>
2011-08-15 14:13:58 +0200

    trading out javascript prompts/confirms for jquery and reducing needless page refreshing
    	modified:   js/dbsite.js
    	modified:   templates/inc/header.html

--- js/dbsite.js
+++ js/dbsite.js
@@ -1,69 +1,91 @@
-// admins get these functions to modify the top nav
-function DeleteTopNav(id){
-   if(confirm('Are you sure you want to permanently delete this category and all pages associated with it?')){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'DeleteTopNav', 'id':id}, function(data){
-         if (Ifparent()){
-            // if you're not on a static page, this will error
-            if(data['id'] == Varparent()){
-               alert("You've deleted the category you're on.  You're being sent to the home page.");
-               navigate('?')
-               return
+// admins get these functions to modify the navigation
+function ClonePrompt(text, id, cm){
+   $("#clonemeprompt").clone().dialog({
+      open: function(e, ui){
+         $(this).attr('id', 'cloneprompt');
+         $('#cloneprompt #clonetext').html(text + '<br>');
+         $('#cloneprompt #saveid').val(id);
+         $('#cloneprompt #savecm').val(cm);
+         $('#cloneprompt #cloneinput').focus();
+         $('#cloneprompt').removeClass('hideme');
+      },
+      close: function(e, ui){
+         $(this).remove();
+      }
+   });
+}
+
+function SubmitPrompt(){
+   var value = $('#cloneprompt #cloneinput').val();
+   if (value){
+      // vars
+      var saveid = $('#cloneprompt #saveid').val();
+      var id = saveid.replace(/\D/g, '')
+      var savecm = $('#cloneprompt #savecm').val();
+
+      if (savecm=='AddSideNav'){
+         // adding a side nav
+         $.getJSON('ajax.py', {'mode':"Home", 'cm':savecm, 'parent':Varparent(), 'name':value}, function(data){
+            alert('Your page has been saved.  You need to refresh to see the page.');
+         });
+      } else {
+         // adding or editing anything other than adding side nav
+         $.getJSON('ajax.py', {'mode':"Home", 'cm':savecm, 'id':id, 'name':value}, function(data){
+            if (id){
+               // edit
+               $('#' + saveid + ' a').html(value);
+            } else {
+               // they have added a new page
+               alert('Your category has been saved.  You need to refresh to see the category.');
             }
+         });
+      }
+   }
+   $('#cloneprompt').remove()
+}
+
+function CloneConfirm(text, id, cm){
+   $("#clonemeconfirm").clone().dialog({
+      open: function(e, ui){
+         $(this).attr('id', 'cloneconfirm');
+         $('#cloneconfirm #clonetext').html(text + '<br>');
+         $('#cloneconfirm #saveid').val(id);
+         $('#cloneconfirm #savecm').val(cm);
+         $('#cloneconfirm #ok').focus();
+         $('#cloneconfirm').removeClass('hideme');
+      },
+      close: function(e, ui){
+         $(this).remove();
+      }
+   });
+}
+
+function SubmitConfirm(){
+   // vars
+   var saveid = $('#cloneconfirm #saveid').val(); //navul2
+   var id = saveid.replace(/\D/g, ''); //2
+   var savecm = $('#cloneconfirm #savecm').val(); //DeleteTopNav
+
+   $('#' + saveid).remove();
+
+   $.getJSON('ajax.py', {'mode':"Home", 'cm':savecm, 'id':id}, function(data){
+      if (savecm=='DeleteTopNav') {
+         // if you're not on a static page, this will error
+         if (Ifparent() && id == Varparent()){
+            $('#cloneconfirm').remove()
+            alert("You've deleted the category you're on.  You're being sent to the home page.");
+            navigate('?')
+            return
          }
-         location.reload(true);
-      });
-   }
-}
-
-function EditTopNav(id, name){
-   var newname = prompt("Please put in the new name for " + name + ".")
-   if(newname){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'EditTopNav', 'id':id, 'name':newname}, function(data){
-         location.reload(true);
-      });
-   }
-}
-
-function AddTopNav(){
-   var name = prompt("Please put in the name for the new category.")
-   if(name){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'AddTopNav', 'name':name}, function(data){
-         location.reload(true);
-      });
-   }
-}
-
-// admins get these functions to modify the side nav
-function DeleteSideNav(id){
-   if(confirm('Are you sure you want to permanently delete this page?')){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'DeleteSideNav', 'id':id}, function(data){
-         if (Ifstaticid()){
-            // if you're not on a static page, this will error
-            if(data['id'] == Varstaticid()){
-               alert("You've deleted the page you're on.  You're being sent to the home page.");
-               navigate('?')
-               return
-            }
+      } else {
+         // if you're not on a static page, this will error
+         if (Ifstaticid() && id == Varstaticid()){
+            $('#cloneconfirm').remove()
+            alert("You've deleted the category you're on.  You're being sent to the home page.");
+            navigate('?')
+            return
          }
-         location.reload(true);
-      });
-   }
-}
-
-function EditSideNav(id, name){
-   var newname = prompt("Please put in the new name for " + name + ".")
-   if(newname){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'EditSideNav', 'id':id, 'name':newname}, function(data){
-         location.reload(true);
-      });
-   }
-}
-
-function AddSideNav(){
-   var name = prompt("Please put in the name for the new page.")
-   if(name){
-      $.getJSON('ajax.py', {'mode':"Home", 'cm':'AddSideNav', 'parent':Varparent(), 'name':name}, function(data){
-         location.reload(true);
-      });
-   }
+      }
+   });
+   $('#cloneconfirm').remove()
 }
